f67a703d4de583dc92aab60062bc9d2cc9540401
Liam Gray <hoxxep@gmail.com>
2024-08-22 16:32:34 +0100

    Add to_string, pipe, and more complete docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,10 @@
-//! Serde JSON Canonicalization
-//!
-//! JSON Canonicalization Scheme [RFC-8785](https://datatracker.ietf.org/doc/html/rfc8785)
-//! defines a JSON serialization scheme to allow using JSON data in
-//! cryptographic operations that rely byte level reproduction of data. It can
-//! be used instead of storing the serialized format as a BASE64 encoded string
-//! or similar packaging, allowing easier handling of JSON formatted data that
-//! can be canonicalized before feeding it to a cyrptographic function.
-//!
+#![doc = include_str!("../README.md")]
+#![warn(missing_docs)]
 
 mod jcs;
+mod util;
 
+#[doc(inline)]
 pub use crate::jcs::*;
+#[doc(inline)]
+pub use crate::util::*;
